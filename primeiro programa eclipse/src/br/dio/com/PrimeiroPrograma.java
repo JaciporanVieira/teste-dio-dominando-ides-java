2ab3ca20c20e336c5a0e455cb6c08dfbff99796c
JaciporanVieira <jaciporavieira@gmail.com>
2023-03-29 00:02:26 -0300

    update

--- primeiro programa eclipse/src/br/dio/com/PrimeiroPrograma.java
+++ primeiro programa eclipse/src/br/dio/com/PrimeiroPrograma.java
@@ -7,7 +7,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		Gato gato = new Gato();
 		Livro livro = new Livro();
-		
+	
 		System.out.println(gato);
 		System.out.println(livro);
 		
@@ -16,7 +16,6 @@ public class PrimeiroPrograma {
 		System.out.println("Hello World! " + (a+b));*/
 	
 	}
-	
 }
 
 class Livro {
